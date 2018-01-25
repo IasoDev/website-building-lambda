c758ae269131b44501a11fbe8682c5ff446bc7b8
ga38seg <v.schrader@tum.de>
2018-01-25 15:02:51 +0100

    use AWS.dynamoDB

--- functions/WebsiteBuilding/src/index.js
+++ functions/WebsiteBuilding/src/index.js
@@ -1,16 +1,11 @@
-'use strict';
-
-console.log('Loading function');
-const doc = require('dynamodb-doc');
-const dynamo = new doc.DynamoDB();
-
-const AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 const s3 = new AWS.S3(); 
 
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     
-    var writeResultsToS3 = (err, results) => {
+/*     var writeResultsToS3 = (err, results) => {
         if ( err ) {
             console.log(err, err.stack);
             callback(err, 'There was an error');
@@ -18,7 +13,7 @@ exports.handler = (event, context, callback) => {
             var params = {Bucket: 'votingdemo', Key: 'data.json', Body: JSON.stringify(results)};
             s3.upload(params, callback);
         }
-    };
+    }; */
     
     //dynamo.scan({ TableName: 'VoteTally', ConsistentRead: true }, writeResultsToS3);
     
@@ -27,7 +22,7 @@ exports.handler = (event, context, callback) => {
 	 TableName: "Requests"
 	};
 
-	dynamo.scan(params, function(err, data) {
+	dynamodb.scan(params, function(err, data) {
 	  if (err) {
 		console.log("Error", err);
 	  }
