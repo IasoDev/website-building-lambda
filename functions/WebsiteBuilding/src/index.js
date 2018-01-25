03588c667d985bb5ab0380ec8119f723864046ae
ga38seg <v.schrader@tum.de>
2018-01-26 00:00:43 +0100

    restructure + log data.Items

--- functions/WebsiteBuilding/src/index.js
+++ functions/WebsiteBuilding/src/index.js
@@ -4,30 +4,20 @@ const s3 = new AWS.S3();
 
 export default function(event, context, callback){
     console.log('Received event:', JSON.stringify(event, null, 2));
-    
-/*     var writeResultsToS3 = (err, results) => {
+
+     var writeResultsToS3 = (err, data) => {
         if ( err ) {
             console.log(err, err.stack);
             callback(err, 'There was an error');
         } else {
-            var params = {Bucket: 'votingdemo', Key: 'data.json', Body: JSON.stringify(results)};
-            s3.upload(params, callback);
+			console.log(JSON.stringify(data.Items));
+            //var params = {Bucket: 'notification-panel', Key: 'data.json', Body: JSON.stringify(data.Items)};
+            //s3.upload(params, callback);
         }
-    }; */
-    
-    //dynamo.scan({ TableName: 'VoteTally', ConsistentRead: true }, writeResultsToS3);
-    
-	var params = {
+    }; 
+    var params = {
 	 //ProjectionExpression: "mapAttr", //This can be used to just select specific keys
 	 TableName: "Requests"
 	};
-
-	dynamodb.scan(params, function(err, data) {
-	  if (err) {
-		console.log("Error", err);
-	  }
-	  else {
-		console.log(JSON.stringify(data));
-		}
-	  });
+    dynamo.scan(params, writeResultsToS3);
 };
